9e500d39e46901edd5f642e4f5a29da29b50b43a
YC99993333 <tinoycl@gmail.com>
2020-04-02 15:38:33 +0800

    Updates

---
+++ precache-manifest.945182814fd9afb61920465bb9629253.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fc37dc497bb15052b1bb7cbef39995",
+    "url": "/React-Todos/index.html"
+  },
+  {
+    "revision": "0f1a3b712fe0e09ab126",
+    "url": "/React-Todos/static/js/2.7c2b7cb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todos/static/js/2.7c2b7cb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78528e2353dffaac6c35",
+    "url": "/React-Todos/static/js/main.c2301cf3.chunk.js"
+  },
+  {
+    "revision": "b1462f15e28294d81b9d",
+    "url": "/React-Todos/static/js/runtime-main.9fde838b.js"
+  }
+]);
